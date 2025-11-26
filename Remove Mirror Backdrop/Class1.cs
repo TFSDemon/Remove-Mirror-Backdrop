3cc7c55fda836aabed5eb230d9493c8dbb79202b
emily <emily@DESKTOP-KTQ1SE4>
2025-11-26 04:08:29 -0500

    Add project files.

---
+++ Remove Mirror Backdrop/Class1.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.tfsdemon.disablemirrorbackdrop", "Disable Mirror Backdrop", "1.0.0")]
+public class DisableMirrorBackdrop : BaseUnityPlugin
+{
+    private GameObject mirrorBackdrop;
+
+    private void Awake()
+    {
+        Logger.LogInfo("Disable Mirror Backdrop mod loaded~");
+    }
+
+    private void Update()
+    {
+        if (mirrorBackdrop == null)
+        {
+            mirrorBackdrop = GameObject.Find("Mirror Backdrop");
+        }
+
+        if (mirrorBackdrop != null && mirrorBackdrop.activeSelf)
+        {
+            mirrorBackdrop.SetActive(false);
+            Logger.LogInfo("Mirror Backdrop was enabled and has been forcibly turned off again.");
+        }
+    }
+}
